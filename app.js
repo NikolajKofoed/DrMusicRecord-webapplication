69f97a7f4c911d737edca462d1a206081f4b5ee3
Nikolaj Kofoed <nik003@edu.zealand.dk>
2024-11-28 10:49:11 +0100

    added filtering

--- app.js
+++ app.js
@@ -4,15 +4,33 @@ const app = Vue.createApp({
     data() {
         return {
             records: [],
+            sortByArtist: null,
+            sortByTitle: null,
         }
     },
     methods: {
         async GetRecords(){
             try {
-                const response = await axios.get(baseUrl);
+                var response = null
+                if(this.sortByArtist != null && this.sortByTitle == null){
+                    response = await axios.get(baseUrl + "?" + `artist=${this.sortByArtist}`)
+                }
+                else if(this.sortByArtist == null && this.sortByTitle != null){
+                    response = await axios.get(baseUrl + "?" + `title=${this.sortByTitle}`)
+                }
+                else if(this.sortByArtist != null && this.sortByTitle != null){
+                    response = await axios.get(baseUrl + "?" + `artist=${this.sortByArtist}&title=${this.sortByTitle}`)
+                }
+                else {
+                    response = await axios.get(baseUrl);
+                }
                 this.records = response.data
             } catch (ex){
                 console.error("fejl ved hentning af records" + ex)
+            }
+            finally{
+                this.sortByArtist = null
+                this.sortByTitle = null
             }
         }
     }
